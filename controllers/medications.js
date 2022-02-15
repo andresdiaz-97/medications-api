339dd755a7e8a9c7e1c7fec74db6a0eeca68e225
andresdiaz-97 <andres.diaz1508@gmail.com>
2022-02-14 22:55:03 -0300

    Goal controllers setup and middleware completed

--- controllers/medications.js
+++ controllers/medications.js
@@ -1,29 +1,44 @@
-exports.getMedications = (req, res, next) => {
-  res.status(200).json({ success: true, message: "Get all the medications" });
-};
+const asyncHandler = require("express-async-handler");
 
-exports.getMedication = (req, res, next) => {
+const getMedications = asyncHandler(async (req, res, next) => {
+  res.status(200).json({ success: true, message: "Get all the medications" });
+});
+
+const getMedication = asyncHandler(async (req, res, next) => {
   res
     .status(200)
     .json({ success: true, message: `Get the medication ${req.params.id}` });
-};
+});
 
-exports.addMedication = (req, res, next) => {
+const addMedication = asyncHandler(async (req, res, next) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res
     .status(200)
     .json({ success: true, message: `Add a medication to the database` });
-};
+});
 
-exports.updateMedication = (req, res, next) => {
+const updateMedication = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: `Update the medication ${req.params.id}`,
   });
-};
+});
 
-exports.deleteMedication = (req, res, next) => {
+const deleteMedication = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: `Medication ${req.params.id} was deleted successfully`,
   });
+});
+
+module.exports = {
+  getMedications,
+  getMedication,
+  addMedication,
+  updateMedication,
+  deleteMedication,
 };
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const medications = require("./routes/medications");
@@ -9,6 +10,13 @@ connectDB();
 
 const app = express();
 
+// Add middleware for json body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/v1/medications", medications);
+
+// Add error middleware
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
